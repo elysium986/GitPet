112dbe79f36803ec24a4a20dd3a5f636d24038db
elysium986 <elysium986@gmail.com>
2014-09-05 22:05:11 +0300

    main test method

--- src/main/java/util/AppUtil.java
+++ src/main/java/util/AppUtil.java
@@ -4,4 +4,8 @@ package util;
  * Created by Alexander Khodakovskyi on 05/09/14.
  */
 public class AppUtil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
 }
